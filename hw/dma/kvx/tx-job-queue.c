a0c6595292c1455566ee02730bfc9ce942a783b9
Luc Michel <lmichel@kalray.eu>
2021-10-26 17:00:26 +0200

    hw/dma/kvx: add the TX job queue logic
    This commit adds the behaviour implementation of the TX job queues.
    The job queue fetches job descriptors from memory and feeds a TX thread
    job cache with it.
    
    Signed-off-by: Luc Michel <lmichel@kalray.eu>
    
    Differential Revision: D16044

--- hw/dma/kvx/tx-job-queue.c
+++ hw/dma/kvx/tx-job-queue.c
@@ -25,6 +25,137 @@
 #include "registers.h"
 #include "internals.h"
 
+/*
+ * Returns the address of the next job descriptor of the queue, given by the
+ * start address, the queue size, and the read pointer.
+ */
+static inline hwaddr get_next_job_desc_addr(KvxDmaTxJobQueue *queue)
+{
+    uint64_t ptr = queue->read_ptr & ((1 << queue->num_slots) - 1);
+
+    return queue->start_addr + (ptr * TX_JOB_DESC_SIZE);
+}
+
+/*
+ * Fetch the next job descriptor from memory.
+ * Return false on load error.
+ */
+static bool fetch_job_desc(KvxDmaState *s, KvxDmaTxJobQueue *queue,
+                           uint64_t *job_desc)
+{
+    hwaddr addr;
+    MemTxResult res;
+
+    addr = get_next_job_desc_addr(queue);
+    res = address_space_read(&s->as, addr, MEMTXATTRS_UNSPECIFIED,
+                             job_desc, TX_JOB_DESC_SIZE);
+
+    return res == MEMTX_OK;
+}
+
+/*
+ * Initialize a job context with data from a job descriptor
+ */
+static void fill_job_from_desc(KvxDmaTxJobContext *job, uint64_t *desc)
+{
+    uint64_t desc_ctrl = desc[R_TX_JOB_DESC_CTRL];
+
+    memcpy(job->parameters, desc, sizeof(job->parameters));
+    job->noc_route_id = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, NOC_ROUTE_ID);
+    job->pgrm_id = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, PGRM_ID);
+    job->comp_queue_id = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, COMP_QUEUE_ID);
+    job->rx_job_push_enabled = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, RX_JOB_PUSH_EN);
+    job->rx_job_queue_id = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, RX_JOB_QUEUE_ID);
+    job->fence_before = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, FENCE_BEFORE);
+    job->fence_after = FIELD_EX64(desc_ctrl, TX_JOB_DESC_CTRL, FENCE_AFTER);
+
+    job->valid = true;
+}
+
+/*
+ * Extract the next job from the queue and fill the thread cache with it.
+ * The thread cache must be empty.
+ *
+ * @return true if a job has been successfully put into the thread cache, false
+ *         otherwise.
+ */
+static bool run_queue(KvxDmaState *s, KvxDmaTxJobQueue *queue,
+                      KvxDmaTxThread *thread)
+{
+    uint64_t job_desc[TX_JOB_DESC_NUM_ELTS];
+    KvxDmaTxJobContext *job = NULL;
+
+    if (!queue->running) {
+        /* This queue is not running */
+        return false;
+    }
+
+    if (queue->valid_write_ptr <= queue->read_ptr) {
+        /* Queue empty */
+        return false;
+    }
+
+    if (!fetch_job_desc(s, queue, job_desc)) {
+        kvx_dma_tx_job_queue_report_error(s, queue,
+                                          KVX_DMA_ERR_TX_JOB_QUEUE_ADDR);
+        return false;
+    }
+
+    /* XXX not sure when this is done */
+    queue->read_ptr++;
+
+    job = kvx_dma_tx_thread_get_cached_job(thread);
+
+    fill_job_from_desc(job, job_desc);
+
+    return true;
+}
+
+static bool run_queues_for_thread(KvxDmaState *s, size_t thread_id)
+{
+    size_t i;
+    KvxDmaTxThread *thread = &s->tx_thread[thread_id];
+
+    for (i = 0; i < KVX_DMA_NUM_TX_JOB_QUEUE; i++) {
+        size_t local_i = (thread->next_queue + i) % KVX_DMA_NUM_TX_JOB_QUEUE;
+        KvxDmaTxJobQueue *queue = &s->tx_job_queue[local_i];
+
+        if (queue->thread_id != thread_id) {
+            /* This queue is not targeting this thread */
+            continue;
+        }
+
+        if (run_queue(s, queue, thread)) {
+            /* Cache successfully filled */
+            thread->next_queue = (local_i + 1) % KVX_DMA_NUM_RX_JOB_QUEUE;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static void run_queues(KvxDmaState *s)
+{
+    size_t i;
+    bool run_threads = false;
+
+    for (i = 0; i < KVX_DMA_NUM_TX_THREAD; i++) {
+        KvxDmaTxThread *thread = &s->tx_thread[i];
+
+        if (kvx_dma_tx_thread_has_cached_job(thread)) {
+            /* A job is already waiting in the cache */
+            continue;
+        }
+
+        run_threads = run_threads || run_queues_for_thread(s, i);
+    }
+
+    if (run_threads) {
+        kvx_dma_run_tx_threads(s);
+    }
+}
+
 static inline void enable_queue(KvxDmaState *s, KvxDmaTxJobQueue *queue)
 {
     if (queue->running) {
@@ -32,6 +163,7 @@ static inline void enable_queue(KvxDmaState *s, KvxDmaTxJobQueue *queue)
     }
 
     queue->running = true;
+    run_queues(s);
 }
 
 void kvx_dma_tx_job_queue_report_error(KvxDmaState *s, KvxDmaTxJobQueue *queue,
@@ -99,6 +231,7 @@ uint64_t kvx_dma_tx_job_queue_read(KvxDmaState *s, size_t id, hwaddr offset,
 
     case A_TX_JOB_QUEUE_LOAD_INCR_VALID_WRITE_POINTER:
         ret = queue->valid_write_ptr++;
+        run_queues(s);
         break;
 
     case A_TX_JOB_QUEUE_READ_POINTER:
@@ -155,6 +288,7 @@ void kvx_dma_tx_job_queue_write(KvxDmaState *s, size_t id, hwaddr offset,
 
     case A_TX_JOB_QUEUE_VALID_WRITE_POINTER:
         queue->valid_write_ptr = value;
+        run_queues(s);
         break;
 
     case A_TX_JOB_QUEUE_READ_POINTER:
